93445ef2fb32aec6ea198adc96caf91264b93dcd
AlanGustav0 <alangustavo946@gmail.com>
2021-10-21 21:28:46 -0300

    Revert "Fisrt Commit"
    This reverts commit ed5f6ab2591884dd31b53f0c2c071fc517893d70.

--- api-server/server.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-
-http.createServer(require('./config/express')).listen(3000,function(){
-    console.log('Servidor escutando na porta: ' + this.address().port)
-});
-
-
-
