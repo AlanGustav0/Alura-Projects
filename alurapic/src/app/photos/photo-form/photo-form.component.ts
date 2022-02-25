538db4223ae184cd918d4436fb5ed48e400a545c
AlanGustav0 <alangustavo946@gmail.com>
2022-02-25 09:39:27 -0300

    Ajuste de alert, component

--- alurapic/src/app/photos/photo-form/photo-form.component.ts
+++ alurapic/src/app/photos/photo-form/photo-form.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { UserService } from 'src/app/core/user/user.service';
+import { AlertService } from 'src/app/shared/components/alert/alert.service';
 import { PhotoService } from '../photo/photo.service';
 
 @Component({
@@ -15,7 +17,7 @@ export class PhotoFormComponent implements OnInit {
   preview: string;
 
   constructor(private formBuilder: FormBuilder, 
-    private photoService: PhotoService,private router: Router) { }
+    private photoService: PhotoService,private router: Router,private alertService:AlertService,private userService:UserService) { }
 
   ngOnInit() {
     this.photoForm = this.formBuilder.group({
@@ -31,7 +33,9 @@ export class PhotoFormComponent implements OnInit {
     const allowComments = this.photoForm.get('allowComments').value;
 
     this.photoService.upload(description,allowComments,this.file)
-    .subscribe(() => this.router.navigate(['']));
+    .subscribe(() => {
+      this.alertService.success('Upload complete',true);
+      this.router.navigate(['/user',this.userService.getUserName()])});
   }
 
   //Método para realizar a conversão em base64 e obter oa dados em formato de string
--- alurapic/src/app/shared/components/alert/alert.service.ts
+++ alurapic/src/app/shared/components/alert/alert.service.ts
@@ -1,39 +1,60 @@
 import { Injectable } from "@angular/core";
+import { NavigationStart, Router } from "@angular/router";
 import { Subject } from "rxjs";
 import { Alert, AlertType } from "./alert";
 
-@Injectable({providedIn:'root'})
-export class AlertService{
+@Injectable({ providedIn: 'root' })
+export class AlertService {
 
     //Este elemento irá emitir o alerta para todos os componentes escritos nele
-    alertSubject:Subject<Alert> = new Subject<Alert>();
+    alertSubject: Subject<Alert> = new Subject<Alert>();
+    keepAfterRouteChange: boolean = false;
 
-
-    success(message:string){
-        this.alert(AlertType.SUCCESS,message);
+    //Aqui no construtor utilizamos o router para obter a rota que esta sendo navegada, se é uma rota de início
+    //Caso seja, e a variável keepAfterRouteChange seja true, então voltamos ela para false apenas, caso contrário,
+    //chamamos o método clear que apaga o objeto de alert passando um valor nulo, com isso teremos a mensagem de alert apenas no componente que estão sendo utilizado.
+    constructor(private router: Router) {
+        router.events.subscribe(event => {
+            if (event instanceof NavigationStart) {
+                if (this.keepAfterRouteChange) {
+                    this.keepAfterRouteChange = false;
+                } else {
+                    this.clear();
+                }
+            }
+        })
     }
 
-    warning(message:string){
-        this.alert(AlertType.WARNING,message);
+    success(message: string, keepAfterRouteChange: boolean = false) {
+        this.alert(AlertType.SUCCESS, message, keepAfterRouteChange);
     }
 
-    danger(message:string){
-        this.alert(AlertType.DANGER,message);
+    warning(message: string, keepAfterRouteChange: boolean = false) {
+        this.alert(AlertType.WARNING, message, keepAfterRouteChange);
     }
 
-    info(message:string){
-        this.alert(AlertType.INFO,message);
+    danger(message: string, keepAfterRouteChange: boolean = false) {
+        this.alert(AlertType.DANGER, message, keepAfterRouteChange);
+    }
+
+    info(message: string, keepAfterRouteChange: boolean = false) {
+        this.alert(AlertType.INFO, message, keepAfterRouteChange);
     }
 
     //Os componentes que quiserem emitir uma mensagem, terão que chamar o método alert()
-    private alert(alertType:AlertType,message:string){
-        this.alertSubject.next(new Alert(alertType,message));
+    private alert(alertType: AlertType, message: string, keepAfterRouteChange: boolean = false) {
+        this.keepAfterRouteChange = keepAfterRouteChange;
+        this.alertSubject.next(new Alert(alertType, message));
     }
 
     /*
     O componente que quiser receber a notificação e ser atualizado, chamará o método getAlert e ficará "ouvindo"
     */
-    getAlert(){
+    getAlert() {
         return this.alertSubject.asObservable();
+    }
+
+    clear() {
+        this.alertSubject.next(null);
     }
 }
