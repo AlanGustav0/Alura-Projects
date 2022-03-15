05b42fe9dc40a2e4606346780179a6633e5697aa
AlanGustav0 <alangustavo946@gmail.com>
2022-03-15 15:54:10 -0300

    functionality replaceUrl to erase API History on browser

--- alurapic/src/app/home/singup/username-password-validator.ts
+++ alurapic/src/app/home/singup/username-password-validator.ts
@@ -2,6 +2,8 @@ import { FormGroup, ValidatorFn } from "@angular/forms";
 
 /*
     Aqui a constante criado do tipo ValidatorGFn recebe uma função que recebe um formGroup e verifica se o username é igual ao passaword, caso seja, retirna null, caso contrário, retorna true;
+
+    Um validador crossfield nada mais é do que uma função. Ela recebe como parâmetro um objeto do tipo 'FormGroup'
 */
 export const userNamePassword: ValidatorFn = (formGroup:FormGroup) => {
 
